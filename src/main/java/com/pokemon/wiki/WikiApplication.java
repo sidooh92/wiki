8748aac90b6bea5eaea37c7d6d8728f5136471b8
robert <robert.piotrowski92@gmail.com>
2018-11-27 18:41:54 +0100

    exmaple of rest template to get pokemon

--- src/main/java/com/pokemon/wiki/WikiApplication.java
+++ src/main/java/com/pokemon/wiki/WikiApplication.java
@@ -2,6 +2,16 @@ package com.pokemon.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import javax.security.cert.X509Certificate;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 public class WikiApplication {
--- src/test/java/com/pokemon/wiki/WikiApplicationTests.java
+++ src/test/java/com/pokemon/wiki/WikiApplicationTests.java
@@ -1,16 +1,34 @@
 package com.pokemon.wiki;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class WikiApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
+		CloseableHttpClient httpClient
+				= HttpClients.custom()
+				.setSSLHostnameVerifier(new NoopHostnameVerifier())
+				.build();
+		HttpComponentsClientHttpRequestFactory requestFactory
+				= new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setHttpClient(httpClient);
 
+		ResponseEntity<String> response
+				= new RestTemplate(requestFactory).exchange(
+				"http://pokeapi.co/api/v2/pokemon/1", HttpMethod.GET, null, String.class);
+
+		System.out.println(response);
+	}
 }
+
